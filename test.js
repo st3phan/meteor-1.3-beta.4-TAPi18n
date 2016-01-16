92db1f93a4b3a7b1b0127252791a058806a2a6ef
Stephan van Opstal <st3phan@gmail.com>
2016-01-16 16:02:49 +0100

    First commit

---
+++ test.js
@@ -0,0 +1,11 @@
+if (Meteor.isClient) {
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
+}
